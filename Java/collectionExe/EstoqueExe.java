4fb244e6d05f2d03895192f92151170611a5a9a3
gustavokill <gacs111111@gmail.com>
2021-03-08 02:43:11 -0300

    exercício de collections

---
+++ Java/collectionExe/EstoqueExe.java
@@ -0,0 +1,26 @@
+package collectionExe;
+import java.util.ArrayList;
+import java.util.Collection;
+public class EstoqueExe
+{
+public static void main(String args[])
+{
+	
+   Collection<String> estoque = new ArrayList();
+estoque.add("Arroz");
+estoque.add("Feijão");
+estoque.add("Açucar");
+estoque.add("Sal");
+estoque.add("Macarrão");
+System.out.println("Meu estoque é de : "+ estoque);
+
+  estoque.remove("Açucar");
+System.out.println("\nEstoque atualizado: "+estoque);
+
+for(String lista : estoque)
+{
+System.out.println("\nLista do estoque: " + lista);
+}
+ }
+  }
+   
